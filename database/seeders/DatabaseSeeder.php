536e13669a1773f25b8ffb655c4a1263fcff0437
wlsfernandes <wlsfernandes@gmail.com>
2026-03-25 15:13:03 -0400

    database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Ramsey\Collection\Set;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,7 +15,9 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             AdminUserSeeder::class,
-            RoleSeeder::class
+            RoleSeeder::class,
+            SettingSeeder::class,
+            PageSeeder::class,
         ]);
     }
 }
